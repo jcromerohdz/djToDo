a8943276d174dd04be735a2c960e2548eb3effb8
jcromerohdz <jcromerohdz@gmail.com>
2024-01-12 11:14:40 -0800

    todo app full functionality create a task, edit a task, mark as done task, mark as undone and delete task

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
+    # Mark as done
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    # Mark as Undone
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+    # Edit Task
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    # Delete Task
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
 ]
